8ce58621a101ce3dc8dc7626d204e88872459c40
shirleyZ <tribble1998@gmail.com>
2025-03-08 17:12:06 +0000

    create search service

---
+++ search-service/src/main/java/com/hmall/search/service/ISearchService.java
@@ -0,0 +1,7 @@
+package com.hmall.search.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmall.search.domain.po.Item;
+
+public interface ISearchService extends IService<Item> {
+}
